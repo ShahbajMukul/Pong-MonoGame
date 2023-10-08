fb6c377c192e52ae262a73036c99222a136a8faf
HadiChaaban <chaabanh@umich.edu>
2023-10-08 15:35:27 -0400

    Pong

---
+++ Globals.cs
@@ -0,0 +1,11 @@
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pong {
+  class Globals {
+    public static SpriteBatch spriteBatch;
+    public static int WIDTH = 640, HEIGHT = 480;
+    public static Texture2D pixel;
+    public static int player1_score, player2_score;
+  }
+}
