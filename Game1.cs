1cbce84e90a9ae58ddab77baf629853a0b552667
Shahbaj Mukul <113660257+ShahbajMukul@users.noreply.github.com>
2023-10-19 16:20:44 -0400

    Bounce Ball Game
    A ball game where you avoid spikes. There is a scoring system where you get points for avoiding spikes. If you die, you can see your score and restart the game.

--- Game1.cs
+++ Game1.cs
@@ -1,70 +1,263 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Pong;
+using System.Collections.Generic;
+using System;
+using System.Runtime.CompilerServices;
 
-namespace Pong;
-
-public class Game1 : Game
+namespace Project1
 {
-    private GraphicsDeviceManager _graphics;
-    Paddle paddle;
-    Paddle paddle2;
-    Ball ball;
-    SpriteFont font;
-
-    public Game1()
+    public class Game1 : Game
     {
-        _graphics = new GraphicsDeviceManager(this);
-        _graphics.PreferredBackBufferWidth = Globals.WIDTH;
-        _graphics.PreferredBackBufferHeight = Globals.HEIGHT;
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
-    }
+        SpriteFont scoreFont;
+        int score;
 
-    protected override void Initialize()
-    {
-        // TODO: Add your initialization logic here
-        paddle = new Paddle(false);
-        paddle2 = new Paddle(true);
-        ball = new Ball();
-        base.Initialize();
-    }
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSpeed;
+        float jumpPower;
 
-    protected override void LoadContent()
-    {
-        Globals.spriteBatch = new SpriteBatch(GraphicsDevice);
-        
-        Globals.pixel = new Texture2D(GraphicsDevice, 1, 1);
-        Globals.pixel.SetData<Color>(new Color[] { Color.White });
-        font = Content.Load<SpriteFont>("Score");
-        // TODO: use this.Content to load your game content here
-    }
+        Vector2 velocity;
+        float gravity;
 
-    protected override void Update(GameTime gameTime)
-    {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+        bool grounded;
 
-        // TODO: Add your update logic here
-        paddle.Update(gameTime);
-        paddle2.Update(gameTime);
-        ball.Update(gameTime, paddle, paddle2);
-        base.Update(gameTime);
-    }
+        bool GameOver;
 
-    protected override void Draw(GameTime gameTime)
-    {
-        GraphicsDevice.Clear(Color.Black);
-        Globals.spriteBatch.Begin();
 
-        Globals.spriteBatch.DrawString(font, Globals.player1_score.ToString(), new Vector2(100, 50), Color.White);
-        Globals.spriteBatch.DrawString(font, Globals.player2_score.ToString(), new Vector2(Globals.WIDTH - 112, 50), Color.White);
 
-        paddle.Draw();
-        paddle2.Draw();
-        ball.Draw();
-        Globals.spriteBatch.End();
+        Texture2D smallSpikeTexture;
+        List<Vector2> smallSpikes;
 
-        base.Draw(gameTime);
+        Random random;
+        double timer;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+        }
+
+    
+
+        protected override void Initialize()
+        {
+            score = 0;  
+
+            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+
+            ballSpeed = 500f;
+            GameOver = false;
+            velocity = Vector2.Zero;
+            gravity = 600f; 
+            jumpPower = -800f;
+
+            random =new Random();
+            timer = 0;
+            smallSpikes = new List<Vector2>();
+            /*smallSpikePosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+            _graphics.PreferredBackBufferHeight -100)*/
+            ;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            scoreFont = Content.Load<SpriteFont>("ScoreFont");
+            ballTexture = Content.Load<Texture2D>("ball");
+            smallSpikeTexture = Content.Load<Texture2D>("small_metal_spike");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            ballPosition.Y += 5f;
+
+
+            // hitbox
+            Rectangle ballRectangle = new Rectangle(
+                (int)ballPosition.X,
+                (int)ballPosition.Y,
+                ballTexture.Width,
+                ballTexture.Height);
+
+            // collision with the spike -> game over
+            for (int i = 0; i < smallSpikes.Count; i++)
+            {
+                Rectangle spikeRectangle = new Rectangle(
+                    (int)smallSpikes[i].X,
+                    (int)smallSpikes[i].Y,
+                    smallSpikeTexture.Width,
+                    smallSpikeTexture.Height);
+
+                if (ballRectangle.Intersects(spikeRectangle))
+                {
+                    GameOver = true;
+                }
+            }
+
+
+            if (ballPosition.Y + ballTexture.Height >= GraphicsDevice.Viewport.Height)
+            {
+                grounded = true;
+                // ball is grounded, reset Y position and Y velocity
+                ballPosition.Y = GraphicsDevice.Viewport.Height - ballTexture.Height;
+                velocity.Y = 0;
+            }
+            else
+            {
+                grounded = false;
+            }
+
+            for (int i = 0; i < smallSpikes.Count; i++)
+            {
+                smallSpikes[i] = new Vector2(smallSpikes[i].X - 1, smallSpikes[i].Y);
+
+                //Remove spike if it's off the screen
+                if (smallSpikes[i].X < -smallSpikeTexture.Width)
+                {
+                    smallSpikes.RemoveAt(i);
+                    score++;
+                    i--;
+                }
+            }
+
+            //Add new spikes every 1-3 seconds
+            timer += gameTime.ElapsedGameTime.TotalSeconds;
+            if (timer >= random.Next(4, 7))
+            {
+                smallSpikes.Add(new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight - smallSpikeTexture.Height));
+                timer = 0;
+            }
+
+
+            var kstate = Keyboard.GetState();
+
+            // jump
+            if ((kstate.IsKeyDown(Keys.Up) || kstate.IsKeyDown(Keys.W) || kstate.IsKeyDown(Keys.Space)) && grounded)
+            {
+                velocity.Y = jumpPower;
+                grounded = false;
+            }
+
+            velocity.Y += gravity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            ballPosition += velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+
+            // restart game
+            if (GameOver)
+            {
+                smallSpikes.Clear();
+                
+
+                if (kstate.IsKeyDown(Keys.R))
+                {
+                    GameOver = false;
+                    score = 0;
+                    ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+                }
+            }
+
+ 
+
+
+            // horizontal speed when on ground
+            if ((kstate.IsKeyDown(Keys.Left) || kstate.IsKeyDown(Keys.A)) &&  grounded)
+            {
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if ((kstate.IsKeyDown(Keys.Right) || kstate.IsKeyDown(Keys.D)) && grounded)
+            {
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            // end
+
+            // horizontal speed when in air
+            if ((kstate.IsKeyDown(Keys.Left) || kstate.IsKeyDown(Keys.A)) && !grounded)
+            {
+                ballPosition.X -= (ballSpeed / 2) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if ((kstate.IsKeyDown(Keys.Right) || kstate.IsKeyDown(Keys.D)) && !grounded)
+            {
+                ballPosition.X += (ballSpeed/2) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            // end
+
+
+            // keep the ball on screen
+            if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width)
+            {
+                ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width;
+            }
+            else if (ballPosition.X < ballTexture.Width)
+            {
+                ballPosition.X =  ballTexture.Width; 
+            }
+
+            if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height)
+            {
+                ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height;
+            }
+            else if (ballPosition.Y < ballTexture.Height)
+            {   
+                ballPosition.Y = ballTexture.Height;
+            }
+
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            _graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            // show score at the end of the game
+            if (GameOver)
+            {
+                string gameOverText = "Game over\nScore: " + score.ToString() + "\nClick 'R' to Restart";
+                Vector2 gameOverSize = scoreFont.MeasureString(gameOverText);
+                _spriteBatch.DrawString(scoreFont, gameOverText, new Vector2(_graphics.PreferredBackBufferWidth / 2 - gameOverSize.X , _graphics.PreferredBackBufferHeight / 2 - gameOverSize.Y / 2 ), Color.White);
+            }
+            else
+            {
+                _spriteBatch.DrawString(scoreFont, "Score: " + score.ToString(), new Vector2(15, 15), Color.Black);
+            }
+
+            // draw ball
+            _spriteBatch.Draw(ballTexture, ballPosition, Color.White);
+
+            // multiple spikes
+            foreach (Vector2 position in smallSpikes)
+            {
+                _spriteBatch.Draw(smallSpikeTexture, position, Color.White);
+            }
+
+            _spriteBatch.End();
+
+
+
+            base.Draw(gameTime);
+        }
+
+
     }
 }
--- Globals.cs
+++ Globals.cs
@@ -1,11 +1,22 @@
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Pong {
-  class Globals {
-    public static SpriteBatch spriteBatch;
-    public static int WIDTH = 640, HEIGHT = 480;
-    public static Texture2D pixel;
-    public static int player1_score, player2_score;
-  }
+namespace Pong
+{
+     class Globals
+    {
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSpeed;
+        bool ballIsAlive;
+
+        Texture2D smallSpikeTexture;
+        Vector2 smallSpikePosition;
+
+    }
 }
